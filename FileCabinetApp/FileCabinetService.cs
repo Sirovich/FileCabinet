c2f51ee43c356ce89702566f6c892272ac010a21
Sirovich <52805632+Sirovich@users.noreply.github.com>
2019-10-12 17:40:14 +0300

    Add guard clauses to CreateRecord method.

--- FileCabinetApp/FileCabinetService.cs
+++ FileCabinetApp/FileCabinetService.cs
@@ -10,6 +10,36 @@ namespace FileCabinetApp
 
         public int CreateRecord(short height, decimal weight, char sex, string firstName, string lastName, DateTime dateOfBirth)
         {
+            if (weight < 0)
+            {
+                throw new ArgumentException("Not valid weight");
+            }
+
+            if (height < 0)
+            {
+                throw new ArgumentException("Not valid height");
+            }
+
+            if (sex == ' ')
+            {
+                throw new ArgumentException("Not valid sex");
+            }
+
+            if (dateOfBirth < new DateTime(1950, 01, 01) || dateOfBirth > DateTime.Now)
+            {
+                throw new ArgumentException("Not valid date of birth");
+            }
+
+            if (firstName == null || firstName.Length < 2 || firstName.Length > 60 || firstName.Trim(' ').Length == 0)
+            {
+                throw new ArgumentNullException(nameof(firstName));
+            }
+
+            if (lastName == null || lastName.Length < 2 || lastName.Length > 60 || lastName.Trim(' ').Length == 0)
+            {
+                throw new ArgumentNullException(nameof(lastName));
+            }
+
             var record = new FileCabinetRecord
             {
                 Id = this.list.Count + 1,
